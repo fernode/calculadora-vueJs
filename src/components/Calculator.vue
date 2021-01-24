579c432074c77d8c6e635be811d1c18e9c1a0df7
Fernando Silva <fernandosilva@hotmail.com>
2021-01-24 19:11:45 -0300

    feat - create calculator component

---
+++ src/components/Calculator.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="calculator"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.calculator {
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+</style>
